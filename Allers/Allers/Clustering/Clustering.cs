0e61a4b20900858be3cefc1b103d064c74cdfed9
RigobertoBolanos <juandy46@hotmail.com>
2018-10-13 15:45:11 -0500

    .

--- Allers/Allers/Clustering/Clustering.cs
+++ Allers/Allers/Clustering/Clustering.cs
@@ -20,6 +20,34 @@ namespace Allers.Clustering
                 Random r = new Random();
                 clusters[i] = new Cluster(clientes.ElementAt(r.Next(0, clientes.Count() - 1)));
             }
+
+            var groupNames = clientes.Select(i => i.GroupName).Distinct().ToList();
+            for (int i = 0; i < groupNames.Count(); i++)
+            {
+                if (!datosClientes.ContainsKey(groupNames[i]))
+                    datosClientes.Add(groupNames[i], i);
+            }
+
+            var cities = clientes.Select(i => i.City).Distinct().ToList();
+            for (int i = 0; i < cities.Count(); i++)
+            {
+                if (!datosClientes.ContainsKey(cities[i]))
+                    datosClientes.Add(cities[i], i);
+            }
+
+            var dptos = clientes.Select(i => i.Dpto).Distinct().ToList();
+            for (int i = 0; i < dptos.Count(); i++)
+            {
+                if (!datosClientes.ContainsKey(dptos[i]))
+                    datosClientes.Add(dptos[i], i);
+            }
+
+            var pymnts = clientes.Select(i => i.PymntGruoup).Distinct().ToList();
+            for (int i = 0; i < dptos.Count(); i++)
+            {
+                if (!datosClientes.ContainsKey(pymnts[i]))
+                    datosClientes.Add(pymnts[i], i);
+            }
         }
     }
 }
