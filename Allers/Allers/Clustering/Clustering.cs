187d653248299516b9daa365759a13f95c71317f
RigobertoBolanos <juandy46@hotmail.com>
2018-10-13 15:42:57 -0500

    .

--- Allers/Allers/Clustering/Clustering.cs
+++ Allers/Allers/Clustering/Clustering.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace Allers.Clustering
 {
     class Clustering
     {
+        public Hashtable datosClientes = new Hashtable();
+        public List<Cliente> clientes = new List<Cliente>();
+        public Cluster[] clusters;
+        public Clustering(List<Cliente> clientes, int numberOfClusters)
+        {
+            clusters = new Cluster[numberOfClusters];
+            for(int i = 0; 0 < numberOfClusters; i++)
+            {
+                Random r = new Random();
+                clusters[i] = new Cluster(clientes.ElementAt(r.Next(0, clientes.Count() - 1)));
+            }
+        }
     }
 }
